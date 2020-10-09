8ad50496a8dc931192973e40c9b7ee7146bb1cc8
Rafael <rafael.andrade.dev0@gmail.com>
2020-10-09 10:02:58 -0300

    Criando página de home

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './page/home.component';
+import { SearchComponent } from './page/search/search.component';
+import { UserCardComponent } from './page/user-card/user-card.component';
+import { FormsModule } from '@angular/forms';
+import { UsersComponent } from './page/users/users.component';
+
+@NgModule({
+  declarations: [HomeComponent, SearchComponent, UserCardComponent, UsersComponent],
+  imports: [CommonModule, HomeRoutingModule, FormsModule],
+})
+export class HomeModule {}
