bb50454bba6d93763ca5f5d63b2f95c2967512a7
Rafael <rafael.andrade.dev0@gmail.com>
2020-10-09 10:01:39 -0300

    Criandlo Componente de Loader

---
+++ src/app/shared/component/loader/loader.component.ts
@@ -0,0 +1,42 @@
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ChangeDetectorRef,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LoadingService } from '@data/service/loading.service';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.css'],
+})
+export class LoaderComponent implements OnInit {
+  loadingSubscription: Subscription;
+  message: string;
+
+  constructor(
+    private readonly loadingService: LoadingService,
+    private _elmRef: ElementRef,
+    private _changeDetectorRef: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    console.log('loader');
+  }
+
+  ngAfterViewInit(): void {
+    this._elmRef.nativeElement.style.display = 'none';
+    this.loadingSubscription = this.loadingService.loading$
+      .pipe()
+      .subscribe((status: boolean) => {
+        this._elmRef.nativeElement.style.display = status ? 'block' : 'none';
+        this._changeDetectorRef.detectChanges();
+      });
+  }
+
+  ngOnDestroy() {
+    this.loadingSubscription.unsubscribe();
+  }
+}
